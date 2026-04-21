34d7805c6c601aa50d0c5202b38f83b2edfbf272
PietroGaspa <136386320+Leffy02@users.noreply.github.com>
2026-04-21 19:28:28 +0200

    modificato l'init in src

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,30 @@
 """ Text-to-SQL application entry point. """
+
+from .classes.clients import *
+from .classes.orchestrators import *
+from .classes.domain_states import *
+from .classes.loaders import *
+from .classes.RAG_service import *
+from .classes.orchestrators import *
+from .classes import prompt_builder
+from .classes import logger
+from .classes import llm_factory
+
+__all__ = [
+    "BaseLLM", 
+    "OpenWebUILLM", 
+    "AzureLLM", 
+    "QueryOrchestrator", 
+    "SchemaOrchestrator", 
+    "QueryStore", 
+    "SchemaStore", 
+    "MySQLClient", 
+    "SQLiteClient", 
+    "llm_factory", 
+    "prompt_builder", 
+    "logger",
+    "QuerySession",
+    "Schema",
+    "Records",
+    "SchemaSource",
+]
--- src/classes/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_dataset import BaseDataset, EvaluationResult, ComparisonResult
-from .bird import BirdDataset
-from .spider import SpiderDataset
-
-__all__ = ["BaseDataset", "EvaluationResult", "ComparisonResult", "BirdDataset", "SpiderDataset"]
